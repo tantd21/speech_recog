3ab2149e5120a799b3993394d27db430e3f81668
thaidoantan <ta090707@gmail.com>
2025-05-15 10:19:48 +0700

    fix deploy error

--- app.py
+++ app.py
@@ -162,4 +162,5 @@ def predict():
             os.remove(temp_wav)
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False) 
